533b539310712d55c97c0659c11b581aaa6c0d47
habibellah <habibayata1@gmail.com>
2022-06-23 18:01:06 +0100

    new pattern : FactoryMethod

--- src/CreationalPatterns/FactoryMethod/Boat.java
+++ src/CreationalPatterns/FactoryMethod/Boat.java
@@ -1,5 +1,5 @@
 package CreationalPatterns.FactoryMethod;
-//this class is one of the concrete class who implement the interface of transport vehicle of your trip and this class define the Boat
+//this class is one of the concrete class who implement the interface of transport vehicle of your trip and this class define the Boat.
 public class Boat implements TransportTrip{
     @Override
     public void transportWay() {
