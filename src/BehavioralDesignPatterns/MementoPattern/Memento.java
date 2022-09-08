e0fb6c1671f4e5c9128dd2ad18ad8073fe79b939
habibellah <habibayata1@gmail.com>
2022-09-08 13:21:58 +0100

    new pattern : Memento Pattern

---
+++ src/BehavioralDesignPatterns/MementoPattern/Memento.java
@@ -0,0 +1,14 @@
+package BehavioralDesignPatterns.MementoPattern;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
---
+++ src/BehavioralDesignPatterns/MementoPattern/Originator.java
@@ -0,0 +1,24 @@
+package BehavioralDesignPatterns.MementoPattern;
+/*
+this program is about text editor we implement the memento pattern to use the undo or restore last
+command
+ */
+public class Originator {
+    private String state;
+
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+    public Memento saveStateToMemento(){
+        return new Memento(state);
+    }
+
+    public void getStateFromMemento(Memento memento){
+        state = memento.getState();
+    }
+}
